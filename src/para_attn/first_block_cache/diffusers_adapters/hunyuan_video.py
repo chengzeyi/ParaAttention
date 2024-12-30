4de7a086fb3a63ae29b5f8f8da6f4db90d1f1639
chengzeyi <ichengzeyi@gmail.com>
2024-12-30 19:30:45 +0800

    fix

--- src/para_attn/first_block_cache/diffusers_adapters/hunyuan_video.py
+++ src/para_attn/first_block_cache/diffusers_adapters/hunyuan_video.py
@@ -43,16 +43,16 @@ def apply_cache_on_transformer(
         return_dict: bool = True,
         **kwargs,
     ) -> Union[torch.Tensor, Dict[str, torch.Tensor]]:
-        if getattr(self, "_is_parallelized", False):
-            with unittest.mock.patch.object(
-                self,
-                "transformer_blocks",
-                cached_transformer_blocks,
-            ), unittest.mock.patch.object(
-                self,
-                "single_transformer_blocks",
-                dummy_single_transformer_blocks,
-            ):
+        with unittest.mock.patch.object(
+            self,
+            "transformer_blocks",
+            cached_transformer_blocks,
+        ), unittest.mock.patch.object(
+            self,
+            "single_transformer_blocks",
+            dummy_single_transformer_blocks,
+        ):
+            if getattr(self, "_is_parallelized", False):
                 return original_forward(
                     hidden_states,
                     timestep,
@@ -64,60 +64,60 @@ def apply_cache_on_transformer(
                     return_dict=return_dict,
                     **kwargs,
                 )
-        else:
-            if attention_kwargs is not None:
-                attention_kwargs = attention_kwargs.copy()
-                lora_scale = attention_kwargs.pop("scale", 1.0)
             else:
-                lora_scale = 1.0
+                if attention_kwargs is not None:
+                    attention_kwargs = attention_kwargs.copy()
+                    lora_scale = attention_kwargs.pop("scale", 1.0)
+                else:
+                    lora_scale = 1.0
 
-            if USE_PEFT_BACKEND:
-                # weight the lora layers by setting `lora_scale` for each PEFT layer
-                scale_lora_layers(self, lora_scale)
-            else:
-                if attention_kwargs is not None and attention_kwargs.get("scale", None) is not None:
-                    logger.warning(
-                        "Passing `scale` via `attention_kwargs` when not using the PEFT backend is ineffective."
-                    )
+                if USE_PEFT_BACKEND:
+                    # weight the lora layers by setting `lora_scale` for each PEFT layer
+                    scale_lora_layers(self, lora_scale)
+                else:
+                    if attention_kwargs is not None and attention_kwargs.get("scale", None) is not None:
+                        logger.warning(
+                            "Passing `scale` via `attention_kwargs` when not using the PEFT backend is ineffective."
+                        )
 
-            batch_size, num_channels, num_frames, height, width = hidden_states.shape
-            p, p_t = self.config.patch_size, self.config.patch_size_t
-            post_patch_num_frames = num_frames // p_t
-            post_patch_height = height // p
-            post_patch_width = width // p
+                batch_size, num_channels, num_frames, height, width = hidden_states.shape
+                p, p_t = self.config.patch_size, self.config.patch_size_t
+                post_patch_num_frames = num_frames // p_t
+                post_patch_height = height // p
+                post_patch_width = width // p
 
-            # 1. RoPE
-            image_rotary_emb = self.rope(hidden_states)
+                # 1. RoPE
+                image_rotary_emb = self.rope(hidden_states)
 
-            # 2. Conditional embeddings
-            temb = self.time_text_embed(timestep, guidance, pooled_projections)
-            hidden_states = self.x_embedder(hidden_states)
-            encoder_hidden_states = self.context_embedder(encoder_hidden_states, timestep, encoder_attention_mask)
+                # 2. Conditional embeddings
+                temb = self.time_text_embed(timestep, guidance, pooled_projections)
+                hidden_states = self.x_embedder(hidden_states)
+                encoder_hidden_states = self.context_embedder(encoder_hidden_states, timestep, encoder_attention_mask)
 
-            encoder_hidden_states = encoder_hidden_states[:, encoder_attention_mask[0].bool()]
+                encoder_hidden_states = encoder_hidden_states[:, encoder_attention_mask[0].bool()]
 
-            hidden_states, encoder_hidden_states = self.call_transformer_blocks(
-                hidden_states, encoder_hidden_states, temb, guidance, image_rotary_emb
-            )
+                hidden_states, encoder_hidden_states = self.call_transformer_blocks(
+                    hidden_states, encoder_hidden_states, temb, guidance, image_rotary_emb
+                )
 
-            # 5. Output projection
-            hidden_states = self.norm_out(hidden_states, temb)
-            hidden_states = self.proj_out(hidden_states)
+                # 5. Output projection
+                hidden_states = self.norm_out(hidden_states, temb)
+                hidden_states = self.proj_out(hidden_states)
 
-            hidden_states = hidden_states.reshape(
-                batch_size, post_patch_num_frames, post_patch_height, post_patch_width, -1, p_t, p, p
-            )
-            hidden_states = hidden_states.permute(0, 4, 1, 5, 2, 6, 3, 7)
-            hidden_states = hidden_states.flatten(6, 7).flatten(4, 5).flatten(2, 3)
+                hidden_states = hidden_states.reshape(
+                    batch_size, post_patch_num_frames, post_patch_height, post_patch_width, -1, p_t, p, p
+                )
+                hidden_states = hidden_states.permute(0, 4, 1, 5, 2, 6, 3, 7)
+                hidden_states = hidden_states.flatten(6, 7).flatten(4, 5).flatten(2, 3)
 
-            if USE_PEFT_BACKEND:
-                # remove `lora_scale` from each PEFT layer
-                unscale_lora_layers(self, lora_scale)
+                if USE_PEFT_BACKEND:
+                    # remove `lora_scale` from each PEFT layer
+                    unscale_lora_layers(self, lora_scale)
 
-            if not return_dict:
-                return (hidden_states,)
+                if not return_dict:
+                    return (hidden_states,)
 
-            return Transformer2DModelOutput(sample=hidden_states)
+                return Transformer2DModelOutput(sample=hidden_states)
 
     def call_transformer_blocks(self, hidden_states, encoder_hidden_states, temb, guidance, image_rotary_emb):
         # 4. Transformer blocks
