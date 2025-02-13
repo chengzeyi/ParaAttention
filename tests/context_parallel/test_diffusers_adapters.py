9bdc4238bad0d725f9c5cd249dbb9e6520627842
chengzeyi <ichengzeyi@gmail.com>
2025-02-13 08:44:03 +0000

    do not use max-autotune-no-cudagraphs in tests

--- tests/context_parallel/test_diffusers_adapters.py
+++ tests/context_parallel/test_diffusers_adapters.py
@@ -43,8 +43,6 @@ class DiffusionPipelineRunner(MPDistRunner):
             return
 
         with torch.cuda.device(self.rank) if device == "cuda" else contextlib.nullcontext():
-            torch.manual_seed(0)
-
             pipe = self.new_pipe(dtype, device)
 
             if parallelize:
@@ -63,12 +61,16 @@ class DiffusionPipelineRunner(MPDistRunner):
                     torch._inductor.config.reorder_for_compute_comm_overlap = True
                 # If cudagraphs is enabled and parallelize is True, the test will hang indefinitely
                 # after the last iteration.
-                pipe.transformer = torch.compile(pipe.transformer, mode="max-autotune-no-cudagraphs")
+                
+                # TODO: Why CogVideoX fails with max-autotune-no-cudagraphs by outputing NAN?
+                pipe.transformer = torch.compile(pipe.transformer)
 
             output_image, warmup_time, inference_time = None, None, None
 
             try:
                 for i in range(2):
+                    torch.manual_seed(0)
+
                     call_kwargs = {}
                     if i == 0:
                         call_kwargs["num_inference_steps"] = 1
